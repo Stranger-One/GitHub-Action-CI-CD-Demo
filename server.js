5ae0b8febe44341ffc5c3d3ef7fb599407e32ec0
Dolamani Rohidas <dolamanirohidas696@gmail.com>
2026-02-28 19:49:33 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello From Express Server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
